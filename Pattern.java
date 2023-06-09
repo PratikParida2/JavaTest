fc2e3ce69c97d0b2155203152ffcf4eaa79da1bd
PratikParida2 <paridapratik709@gmail.com>
2023-06-09 20:27:47 +0530

    working in new branch

---
+++ Pattern.java
@@ -0,0 +1,24 @@
+public class Pattern
+{
+    public static void main(String[] args) 
+    {
+        int n=5;
+	  System.out.println("pratik");
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" "+" ");
+            }
+            for(int k=i;k>=1;k--)
+            {
+                System.out.print(k+" ");
+            }
+            for(int l=2;l<=i;l++)
+            {
+                System.out.print(l+" ");
+            }
+            System.out.println();
+        }
+    }
+}
