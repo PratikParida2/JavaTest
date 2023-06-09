5035df5f460ea59a253227d9c99ac5a1badc6b9c
PratikParida2 <paridapratik709@gmail.com>
2023-06-09 21:34:46 +0530

    second commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum 
+{
+    public static void main(String[] args) 
+    {
+        int a=12;
+        int b=24;
+        int su=a+b;
+        System.out.println(su);  
+        
+    }    
+}
